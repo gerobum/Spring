8d2b2eb45e09ba5e8fd0012e2f38ae660b9bc306
yvan <gerobum@gmail.com>
2018-09-07 23:28:39 +0200

    Ajout du formulaire

--- PremierTutoSpringFramework/src/main/java/fr/miage/premiertutospringframework/SampleController.java
+++ PremierTutoSpringFramework/src/main/java/fr/miage/premiertutospringframework/SampleController.java
@@ -7,6 +7,9 @@ package fr.miage.premiertutospringframework;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -35,4 +38,14 @@ public class SampleController {
         return "greeting";
     }
 
+    @GetMapping("/form")
+    public String greetingForm(Model model) {
+        model.addAttribute("form", new Greeting());
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        return "result";
+    }
 }
